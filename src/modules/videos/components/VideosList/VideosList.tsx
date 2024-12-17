c5fe5730a149eb289f278951a1422e7a83844234
Maxim Zass <maxim-zasss@yandex.ru>
2024-12-17 23:47:17 +0300

    fix videos list

--- src/modules/videos/components/VideosList/VideosList.tsx
+++ src/modules/videos/components/VideosList/VideosList.tsx
@@ -19,54 +19,54 @@ export const VideosList: FC<VideosListProps> = (props) => {
   const getVideosQuery = useQuery({
     queryKey: [VIDEOS_QUERY_KEY, { page, size: VIDEOS_PER_PAGE, heroName }],
     queryFn: () => getVideos({ page, size: VIDEOS_PER_PAGE, hero: heroName }),
-    enabled: false,
-    placeholderData: {
-      data: {
-        content: [
-          {
-            id: 1,
-            title: 'cow.mp4',
-            description: 'Это видео о корове',
-            category: null,
-            thumbnailUrl: 'https://goo.su/O0wyP',
-            hlsLink: null,
-          },
-          {
-            id: 3,
-            title: 'Lies of P 2024.12.03 - 01.11.15.02.DVR.mp4',
-            description: 'Описание видео Lies of P',
-            category: null,
-            thumbnailUrl: 'https://goo.su/O0wyP',
-            hlsLink: null,
-          },
-        ],
-        pageable: {
-          pageNumber: 0,
-          pageSize: 10,
-          sort: {
-            empty: false,
-            sorted: true,
-            unsorted: false,
-          },
-          offset: 0,
-          paged: true,
-          unpaged: false,
-        },
-        last: true,
-        totalPages: 2,
-        totalElements: 2,
-        size: 10,
-        number: 0,
-        sort: {
-          empty: false,
-          sorted: true,
-          unsorted: false,
-        },
-        first: true,
-        numberOfElements: 2,
-        empty: false,
-      },
-    } as any,
+    // enabled: false,
+    // placeholderData: {
+    //   data: {
+    //     content: [
+    //       {
+    //         id: 1,
+    //         title: 'cow.mp4',
+    //         description: 'Это видео о корове',
+    //         category: null,
+    //         thumbnailUrl: 'https://goo.su/O0wyP',
+    //         hlsLink: null,
+    //       },
+    //       {
+    //         id: 3,
+    //         title: 'Lies of P 2024.12.03 - 01.11.15.02.DVR.mp4',
+    //         description: 'Описание видео Lies of P',
+    //         category: null,
+    //         thumbnailUrl: 'https://goo.su/O0wyP',
+    //         hlsLink: null,
+    //       },
+    //     ],
+    //     pageable: {
+    //       pageNumber: 0,
+    //       pageSize: 10,
+    //       sort: {
+    //         empty: false,
+    //         sorted: true,
+    //         unsorted: false,
+    //       },
+    //       offset: 0,
+    //       paged: true,
+    //       unpaged: false,
+    //     },
+    //     last: true,
+    //     totalPages: 2,
+    //     totalElements: 2,
+    //     size: 10,
+    //     number: 0,
+    //     sort: {
+    //       empty: false,
+    //       sorted: true,
+    //       unsorted: false,
+    //     },
+    //     first: true,
+    //     numberOfElements: 2,
+    //     empty: false,
+    //   },
+    // } as any,
   })
   const results = getVideosQuery.data?.data
   const videos = results?.content
